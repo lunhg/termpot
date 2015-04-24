e15f73541fdfc58a700a846a4b140bc6e384c6ea
jahpd <gcravista@gmail.com>
2015-04-24 18:51:43 -0300

    adicionado branch

---
+++ README.md
@@ -0,0 +1,30 @@
+# Termpot 
+
+É um projeto clone do [wavepot](http://www.wavepot.com) com uma diferença de abordagem; ao invés de escrevermos arquivos, escreve-se diretamente comandos.
+
+# Instalando
+
+~~~.shell
+$ git clone https://www.github.com/jahpd/termpot.git
+~~~
+
+# Rodando
+
+Pode-se rodar com um simples servidor; 
+
+- node.js
+
+~~~.shell
+$ npm install simple-server
+$ simple-server
+~~~
+
+- Python
+
+~~~.shell
+$ python -m SimpleHTTPServer 3000
+~~~
+
+# Usando
+
+O termpot é baseado no Ptty; digite help para mais informações. Ali coloquei um pequeno tutorial interativo.
---
+++ wavepot-runtime.js
@@ -0,0 +1,146 @@
+
+// quick implementation of the wavepot script playback engine
+
+var contextClass = (window.AudioContext || window.webkitAudioContext)
+function WavepotRuntime(context, bufferSize, channels) {
+	this.code = '';
+	this.scope = {};
+	this.time = 0;
+	this.context = context || new contextClass();
+	this.playing = false;
+	this.bufferSize = bufferSize || 1024;
+        this.channels = channels || 2
+	this.scriptnode = this.context.createScriptProcessor(this.bufferSize, 0, this.channels);
+        this.worker = new Worker('recorderWorker.js');
+        this.worker.postMessage({
+          command: 'init',
+          config: {
+            sampleRate: this.context.sampleRate
+          }
+        });
+        this.recording = false;
+}
+
+WavepotRuntime.prototype.init = function(callback){
+    var _this = this;
+    // Maquina virtual de DSP
+    this.scriptnode.onaudioprocess = function(e) {
+	// Sistema estereofonico de 2 canais
+   	var out = [
+    	    e.outputBuffer.getChannelData(0),
+            e.outputBuffer.getChannelData(1)
+    	];
+        	
+	// Tempo discretizado
+	var f = 0, t = 0, td = 1.0 / _this.context.sampleRate;
+        	
+   	// A cada janela temporal, o valor numerico
+   	// de amplitude vai ser atualizado
+	if (_this.scope && _this.scope.dsp && _this.playing) {
+            t = _this.time;
+            for (var i = 0; i < out[0].length; i++) {
+        	
+        	// função definida dinamicamente
+        	f = _this.scope.dsp(t);
+        		
+        	// Se a funcao retornar um número
+        	// utilizar ele nos dois canais
+        	// Se a funcao retornar um Array
+        	// de dois valores, separar nos canais
+		if(typeof(f) === 'number'){
+		    out[0][i] =  f
+        	    out[1][i] =  f
+		}
+		else if (typeof(f) === 'object'){
+		    out[0][i] =  f[0]
+        	    out[1][i] =  f[1]
+		}
+			
+		// Incrementar o tempo
+        	t += td;
+            }
+            _this.time = t;
+	
+	    // Continuar o processamento se nada for atualizado
+	} else {
+            for (var i = 0; i < out[0].length; i++) {
+		out[0][i] = f[0] | f
+		out[1][i] = f[1] | f
+    	    }
+	}
+  	
+  	// Gravar, se for solicitado
+	if (_this.recording){
+	    _this.worker.postMessage({
+		command: 'record',
+		buffer: out
+	    });
+	}
+    }
+    this.scriptnode.connect(_this.context.destination);
+}
+
+WavepotRuntime.prototype.compile = function(code) {
+    // console.log('WavepotRuntime: compile', code);
+    this.code = code;
+    var ee = null;
+    var newscope = new Object();
+    try {
+	var f = new Function('var sampleRate=' + this.context.sampleRate+ ';\n\n' + code + '\n\nthis.dsp = dsp;');
+	console.log(f);
+	var r = f.call(newscope);
+	console.log(r);
+    } catch(e) {
+	console.log(e);
+	ee = e;
+    }
+    console.log('WavepotRuntime: compiled', newscope);
+    if (newscope && typeof(newscope.dsp) == 'function') {
+	this.scope = newscope;
+	return true;
+    } else {
+	if(ee){
+	    return ee.stack.toString();
+	}
+	return false;
+    }
+}
+
+WavepotRuntime.prototype.play = function() {
+	// console.log('WavepotRuntime: play');
+	this.playing = true;
+}
+
+WavepotRuntime.prototype.stop = function() {
+	// console.log('WavepotRuntime: stop');
+	this.playing = false;
+        this.recording = false;
+}
+
+WavepotRuntime.prototype.reset = function() {
+	// console.log('WavepotRuntime: reset');
+	this.time = 0;
+}
+
+WavepotRuntime.prototype.record = function(b){
+        this.recording = b;
+}
+
+WavepotRuntime.prototype.clear = function(){
+        this.worker.postMessage({ command: 'clear' });
+}
+
+WavepotRuntime.prototype.exportWAV = function(cb){
+      type =  'audio/wav';
+      if (!cb) throw new Error('Callback not set');
+      this.worker.postMessage({
+        command: 'exportWAV',
+        type: type
+      });
+      this.worker.onmessage = function(e){
+        var blob = e.data;
+        cb(blob);
+      }
+}
+
+
